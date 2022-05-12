93a797a67c13e72fa6b48eb733603af14a060da4
Matheus Ed <matheus.lafeta.moc@gmail.com>
2022-05-12 11:33:57 -0300

    Imprimindo media do estudante

--- estudante.js
+++ estudante.js
@@ -1,6 +1,21 @@
-let nomeAluno = 'Jhon Doe'
+const nomeAluno = 'Jhon Doe'
 let idadeAluno = 20
-let nota1 = 7
-let nota2 = 8.5
-let nota3 = 6.9
+const nota1 = 7
+const nota2 = 8.5
+const nota3 = 6.9
 const mediaAluno = (nota1*4+nota2*3+nota3*3)/(4+3+3)
+console.log(`Nome do Aluno: ${nomeAluno}\nIdade do aluno: ${idadeAluno}\nMedia do aluno: ${mediaAluno}`)
+
+const passou = media => {
+    //assumindo nota máxima de 10
+    if (7 <= media && media <= 10) {
+        console.log('Aluno aprovado')
+    }else if (media > 0 && media<7) {
+        console.log('Aluno reprovado')
+    }else if (media === 0) {
+        console.log('Aluno zerou')
+    }else {
+        console.log('Erro na entrada de dados!')
+    }
+}
+passou(mediaAluno)
